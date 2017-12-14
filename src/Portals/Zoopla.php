fb491c7960b744d9bdb5458528fa91c0d5ed804f
Daniel Griffiths <daniel.griffiths.jobs@gmail.com>
2017-12-14 00:36:15 +0000

    Start development on Zoopla class

--- src/Portals/Zoopla.php
+++ src/Portals/Zoopla.php
@@ -5,6 +5,22 @@ namespace DanielGriffiths\PortalAmalgamator\Portals;
 class Zoopla extends AbstractPortal implements PortalInterface 
 {
 	/**
+	 * The base search uri for the portal.
+	 * 
+	 * @var string
+	 */
+	protected $baseUri = 'https://www.zoopla.co.uk/to-rent/property/bristol/';
+
+	/**
+	 * Associate the portals query string parameters with our custom filters.
+	 * 
+	 * @var Array
+	 */
+	protected $filterAssociations = [
+
+	];
+
+	/**
 	 * Search for properties based on the specified filters.
 	 *
 	 * @param  array $filters
@@ -12,7 +28,17 @@ class Zoopla extends AbstractPortal implements PortalInterface
 	 */	
 	public function search(array $filters = []) : array 
 	{
-		return [];
+		return $this->request('GET', $this->createQueryString($filters))
+		->filter('.listing-results-wrapper')->each(function ($node) {
+		    return [
+				'image' => $node->filter('.photo-hover > img')->attr('src'),
+				// 'title' => $node->filter('.propertyCard-title')->text(),
+				'address' => $node->filter('.listing-results-address')->text(),
+				// 'description' => $node->filter('.propertyCard-description')->text(),
+				// 'price' => $node->filter('.propertyCard-priceValue')->text(),
+				// 'link' => $node->filter('.propertyCard-link')->attr('href')
+			];
+		});
 	}
 
 	public function details() : array
